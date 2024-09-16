45ea4b17da009e59fa91884a47e7fe9a3407c685
AryaGilangP <aryagilang05@gmail.com>
2024-09-16 20:49:58 +0700

    memperbaiki penulisan dan font

--- the-diecast-shop/main/views.py
+++ the-diecast-shop/main/views.py
@@ -1,8 +1,16 @@
+from django.shortcuts import render
+
 def show_main(request):
     context = {
         'app': 'The Diecast Store',
         'name': 'Arya Gilang Prasetya',
-        'class': 'PBP F'
+        'class': 'PBP F',
+        'price': '50.000',
+        'description':'model ini memiliki warna hitam pekat, dengan detail berupa lampu depan dan belakang.\n'
+                        'model ini juga memiliki fitur untuk membuka dan menutup pintu samping dan kap mesinnya.',
+        'model_number':'12345',
+        'user_reviews':'produk mobil diecast ini sangat bagus dan memiliki detail yang menarik. barang ori dan berkualitas bintang lima.'
+        
     }
 
     return render(request, "main.html", context)
